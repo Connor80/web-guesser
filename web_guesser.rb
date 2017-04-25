62c827721f644cd8ddbcf625a739722694e45b15
Connor80 <Connor@CPU.local>
2017-04-24 22:35:05 -0500

    generates random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+$number = rand(100)
 
 get '/' do
-  "Hello, World!"
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => $number, :message => message}
+end
+
+def check_guess(guess)
+  if guess > $number
+    if guess > $number + 5
+      "Way too high!"
+    else
+      "Too high."
+    end
+
+  elsif guess < $number
+    if guess < $number - 5
+      "Way too low!"
+    else
+      "Too low."
+    end
+
+  elsif guess == $number
+    "Correct!"
+  end
+
 end
